81f00e2ba65d1a87663dace89b5cf065e41221bf
elsehow <yes@cosmopol.is>
2015-11-18 15:34:35 -0800

    cant quite figure out how to interleave streams in the way i'd like to

--- bundledown.js
+++ bundledown.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
 // TODO test with absolute paths
+// TODO should work with e.g. '../../file.md'
 
 var fs            = require('fs')
   , path          = require('path')
   , argv          = require('minimist')(process.argv.slice(2))
   , replaceStream = require('replacestream')
+  , includeRegex  = /@include\('.+'\)/g
 
 function baseDirOf (p) {
   return path.dirname(p)
@@ -27,39 +29,40 @@ function write (f) {
   return fs.createWriteStream(pathTo(f))
 }
 
+// extract relative paths from include steatement
+function relPath (includeStatement) {
+  return includeStatement.match(/'.+'/g)[0].split('\'')[1]
+}
+
 function bundledown (instream, outstream, baseDir) {
 
-  function replaceFn () {
-
-    // the paths in the require statements are relative to the file being req'd
-    function getRelPath (reqStatement) {
-      return reqStatement
-        // get the './path/whatever.md' from req statement
-        .match(/'.+'/g)[0]
-        // get the path between the quotes
-        .split('\'')[1]
-    }
-
-    function getAbsPath (relativePath) {
+  // this fn is specific to a particular `baseDir`
+  function getFilePath (includeStatement) {
+    // turn relative paths into absolute paths 
+    function absPath (relativePath) {
       // remove leading '.' ('./my-path' becomes '/my-path')
       var p = relativePath.slice(1)
       // join to the base dir
       return path.join(baseDir, p)
     }
-
-    var reqExp  = arguments[0]
-    var relPath = getRelPath(reqExp)
-    // TODO should work with e.g. '../../file.md'
-    var absPath = getAbsPath(relPath)
-    // call bundledown recursively on this path
-    // TODO -- how do we 'interrupt' the stream currently being written?
-    bundledown(read(absPath), outstream, baseDirOf(absPath))
+    // return the absolute filepath from the includ estatement 
+    return absPath(relPath(includeStatement))
   }
 
-  instream
-    .pipe(replaceStream(/@include\('.+'\)/g, replaceFn))
-    .pipe(outstream)
+  // this is run everytime we find an include statement
+  function replaceFn (includeStatement) {
+    var absPath = getFilePath(includeStatement, baseDir)
+    // call bundledown recursively on this path
+    instream.pause()
+    bundledown(read(absPath), outstream, baseDirOf(absPath))
+    instream.resume()
+  }
+
+  var rs = replaceStream(includeRegex, replaceFn)
+
+  instream.pipe(rs).pipe(outstream)
 }
+
 
 // usage: `bundledown my-file.md -o bundle.md`
 var input   = argv._[0]
