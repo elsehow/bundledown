88a968f2c500609fb6e1f4364f4c489265a9549b
elsehow <yes@cosmopol.is>
2015-11-19 16:59:36 -0800

    finally, things are being ordered correctly, at least in my tiny test-case

--- bundledown.js
+++ bundledown.js
@@ -6,15 +6,16 @@
 var fs            = require('fs')
 , path          = require('path')
 , argv          = require('minimist')(process.argv.slice(2))
-, replaceStream = require('replacestream')
+, through       = require('through')
+//, replaceStream = require('replacestream')
 , includeRegex  = /@include\('.+'\)/g
-
-function baseDirOf (p) {
-  return path.dirname(p)
-}
 
 function startsWith (str1, str2) {
   return str1[0] === str2
+}
+
+function baseDirOf (p) {
+  return path.dirname(p)
 }
 
 function pathTo (f) {
@@ -29,39 +30,79 @@ function write (f) {
   return fs.createWriteStream(pathTo(f))
 }
 
-// extract relative paths from include steatement
-function relPath (includeStatement) {
-  return includeStatement.match(/'.+'/g)[0].split('\'')[1]
+// return absolute filepath from include statement, given a root directory
+//
+//    getFilePath("@include('./my/path.md'", 'chapter1')
+//    > 'chapter1/my/path.md'
+//
+function getFilePath (inclStatement, baseDir) {
+
+  //ex.
+  //    relPath('@include('./my/path')
+  //    > './my/path'
+  function relPath (inclStatement) {
+    return inclStatement.match(/'.+'/g)[0].split('\'')[1]
+  }
+  
+  // turn relative paths into absolute paths 
+  function absPath (relativePath, baseDir) {
+    // remove leading period
+    var p = relativePath.slice(1)
+    return path.join(baseDir, p)
+  }
+  
+  return absPath(
+      relPath(inclStatement)
+    , baseDir)
 }
 
-function bundledown (instream, outstream, baseDir) {
+function recursivelyParseBuffer (buffer, baseDirectory, thru) {
 
-  // this fn is specific to a particular `baseDir`
-  function getFilePath (includeStatement) {
-    // turn relative paths into absolute paths 
-    function absPath (relativePath) {
-      // remove leading '.' ('./my-path' becomes '/my-path')
-      var p = relativePath.slice(1)
-        // join to the base dir
-        return path.join(baseDir, p)
-    }
-    // return the absolute filepath from the includ estatement 
-    return absPath(relPath(includeStatement))
+  function recurse (splitText1, includeStatement, splitText2) {
+
+    // part of the buffer that comes before include statement
+    // push it to the main buffer
+    thru.queue(new Buffer(splitText1))
+
+    // include statement itself -
+    // open the file it references,
+    // and run each chunk of that through the same routine,
+    // each time referring to our parent buffer here
+    var p       = getFilePath(includeStatement, baseDirectory)
+    var base    = baseDirOf(p)
+
+    read(p)
+      .pipe(through(function (b, _, newNext) {
+        //console.log('hiiii', b.toString())
+        recursivelyParseBuffer(b, base, thru)//, next)
+        //newNext()
+      }, function () {
+        // part of buffer that comes after include statement
+        thru.queue(new Buffer(splitText2))
+      }))
+
   }
 
-  // this is run everytime we find an include statement
-  function replaceFn (includeStatement) {
-    var absPath = getFilePath(includeStatement, baseDir)
-    bundledown(read(absPath), outstream, baseDirOf(absPath))
-    return ''
+  var text      = buffer.toString()
+  //console.log('hi', text)
+  var pieces    = text.split(includeRegex)
+  var matches   = text.match(includeRegex)
+  if (matches) {
+    recurse(pieces[0], matches[0], pieces[1])
   }
- 
-  var rs = replaceStream(includeRegex, replaceFn)
-  instream.pipe(rs).pipe(outstream)
+  else
+    thru.queue(buffer)
 
+  //next()
 }
 
 // usage: `bundledown my-file.md -o bundle.md`
-var input   = argv._[0]
-var output  = argv.o ? write(argv.o) : process.stdout
-bundledown(read(input), output, baseDirOf(input))
+var infile    = argv._[0]
+var outstream = argv.o ? write(argv.o) : process.stdout
+
+read(infile)
+  .pipe(through(function (buff, _, next) {
+    recursivelyParseBuffer(buff, baseDirOf(infile), this)
+  }, function (cb) {
+    //do nothin
+  })).pipe(outstream)
